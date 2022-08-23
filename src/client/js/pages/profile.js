5fa1f120cc92fd54ce7bc594c0e8d756424976ef
yjg <yjglab@yjg.local>
2022-08-24 04:22:10 +0900

    프로필 폼 edit mode

--- src/client/js/pages/profile.js
+++ src/client/js/pages/profile.js
@@ -1,3 +1,14 @@
-// setTimeout(() => {
-//   document.querySelector(".fa-circle-check").style.visibility = "hidden";
-// }, 2000);
+const $profileEditBtn = document.querySelector(".profile-edit-btn");
+function handleProfileEditBtn() {
+  document.querySelectorAll(".edit-mode-element-show").forEach((v) => {
+    v.style.visibility = "visible";
+    v.style.opacity = 1;
+  });
+  document.querySelectorAll(".edit-mode-element-textbox").forEach((v) => {
+    v.style.backgroundColor = "white";
+    v.style.border = "2px solid #333d66";
+    v.style.paddingLeft = "15px";
+    v.style.pointerEvents = "auto";
+  });
+}
+$profileEditBtn.addEventListener("click", handleProfileEditBtn);
